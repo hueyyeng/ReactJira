d9fd97dff310fa1e9e96afdf3f1ecd344b11b0d4
Huey Yeng <huey.yeng@jewelpaymentech.com>
2020-02-12 18:39:56 +0800

    Updates

---
+++ precache-manifest.ea5ef3cf6d091b805bf2b76350beba1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbce3d9315d85ba59c90bf26add7950",
+    "url": "/ReactJira/index.html"
+  },
+  {
+    "revision": "fbd9605a486b77cc1b8b",
+    "url": "/ReactJira/static/js/2.21bd42f5.chunk.js"
+  },
+  {
+    "revision": "be326db8c8d9b3aa36d942e163e338e7",
+    "url": "/ReactJira/static/js/2.21bd42f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efff7def8c02b734b897",
+    "url": "/ReactJira/static/js/main.57c2286f.chunk.js"
+  },
+  {
+    "revision": "0f09750589d1e1083e0c",
+    "url": "/ReactJira/static/js/runtime-main.ded9d867.js"
+  },
+  {
+    "revision": "4a4f982321c592f12ac6bee35d67447f",
+    "url": "/ReactJira/static/media/jpt_logo2.4a4f9823.png"
+  }
+]);
